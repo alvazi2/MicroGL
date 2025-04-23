1800235e6e287bfc32be9ce8f50aa3b8591ef446
alvazi2 <62181531+alvazi2@users.noreply.github.com>
2025-04-23 16:06:41 -0700

    Some renaming for better clarity

--- src/bank_account.py
+++ src/bank_account.py
@@ -4,16 +4,16 @@ import json
 class BankAccounts:
     def __init__(self, property_file_path: str):
         self.json_file_path = property_file_path
-        self.bank_accounts = self._read_bank_accounts_properties()
+        self.bank_accounts = self._read_bank_accounts()
 
-    def _read_bank_accounts_properties(self):
+    def _read_bank_accounts(self):
         with open(self.json_file_path, "r") as file:
             return json.load(file)
 
-    def get_bank_account_properties(self, bank_account_code: str) -> dict:
-        bank_account_properties = self.bank_accounts["bankAccountProperties"].get(bank_account_code)
-        if bank_account_properties:
-            return bank_account_properties
+    def get_bank_account(self, bank_account_code: str) -> dict:
+        bank_account = self.bank_accounts["bankAccounts"].get(bank_account_code)
+        if bank_account:
+            return bank_account
         else:
             raise ValueError(f"No properties found for bank account code: {bank_account_code}")
 
@@ -21,7 +21,7 @@ class BankAccounts:
 class BankAccount:
     def __init__(self, bank_accounts: BankAccounts, bank_account_code: str):
         self.bank_account_code = bank_account_code
-        self.properties = bank_accounts.get_bank_account_properties(bank_account_code)
+        self.properties = bank_accounts.get_bank_account(bank_account_code)
 
     def get_gl_mapping_for_search_string(self, search_string: str, bank_transaction_category: str) -> dict:
         for gl_mapping in self.properties["glMapping"]:
